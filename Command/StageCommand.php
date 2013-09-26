9f30cf9e764656e9579362398317729e55d9125b
Krystian Kuczek <krystian@kuczek.eu>
2013-09-26 13:24:43 +0200

    Staging, translations

--- Command/StageCommand.php
+++ Command/StageCommand.php
@@ -1,41 +1,43 @@
 <?php
 
-namespace Hexmedia\KnockoutBootstrapBundle\Command;
+namespace Hexmedia\AdministratorBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Hexmedia\BootstrapBundle\File\Symlink;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
-class CompileCommand extends ContainerAwareCommand {
+class StageCommand extends ContainerAwareCommand {
 
 	protected function configure() {
 		parent::configure();
 
 		$this
-			->setName('hexmedia:knockout-bootstrap:compile')
-			->setDescription("Compiles knockout-bootstrap bundle")
+			->setName('hexmedia:stage')
+			->setDescription("Prepares version for staging")
 		;
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$this->executeCommand("/usr/bin/grunt", 300, false);
-		$this->executeCommand("/usr/local/bin/grunt", 300, false);
+        $this->getApplication()->find("bazinga:expose-translation:dump")->run($this->getDefaultInput($input), $output);
+        $this->getApplication()->find("fos:js-routing:dump")->run($this->getDefaultInput($input), $output);
+        $this->getApplication()->find("fos:js-routing:dump")->run($this->getDefaultInput($input), $output);
+        $this->getApplication()->find("assets:install")->run($this->getDefaultInput($input), $output);
+        $this->getApplication()->find("assetic:dump")->run($this->getDefaultInput($input), $output);
+        $this->getApplication()->find("cache:clear")->run($this->getDefaultInput($input), $output);
 	}
 
-	protected function executeCommand($cmd, $timeout = 300, $required = true) {
-		$process = new Process("$cmd", $this->getContainer()->get('kernel')->locateResource("@HexmediaKnockoutBootstrapBundle/Resources/knockout-bootstrap"), null, null, $timeout);
-		$process->run(function ($type, $buffer) {
-				echo $buffer;
-			});
-		if ($required && !$process->isSuccessful()) {
-			throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
-		}
-	}
+    private function getDefaultInput(InputInterface $input) {
+//        $newInput = new ArgvInput();
+
+//        $newInput->setOption("env", $input->getOption("env"));
+
+        return $input; //$newInput;
+    }
 
 }
 
