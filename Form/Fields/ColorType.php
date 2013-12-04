d55f80d9dfe667b93b45aa942897202e31fb494c
Krystian Kuczek <krystian@kuczek.eu>
2013-12-04 12:18:31 +0100

    ColorPicker.

---
+++ Form/Fields/ColorType.php
@@ -0,0 +1,14 @@
+<?php
+namespace Hexmedia\AdministratorBundle\Form\Fields;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class DatepickerType
+ * @package Hexmedia\AdministratorBundle\Form\Fields
+ */
+class ColorType extends AbstractType
+{
+
+}
